5aea957865ced5c27e57870efb54529c60405947
Joe Egan <joeegan2202@gmail.com>
2019-10-20 17:44:27 -0400

    Next iter

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ http.createServer( function (req, res) {
 		// parse name and hash to id
 		// parse game room and determine if it's available.
 		// post id and add id to game list
-		
+
 		console.log(`Establishing name: ${qdat.name}, Room: ${qdat.room}`);
 		let id = sha(qdat.name);
 
@@ -33,7 +33,10 @@ http.createServer( function (req, res) {
 					dealing: false,
 					turn: {id: 0, name: ""},
 					mod: 1, // For game: Singles, doubles, or triples
-				}
+					pile: [],
+					stock: [],
+				},
+				requests: [],
 			});
 			res.write('room established:room ' + code);
 		}
@@ -51,7 +54,7 @@ http.createServer( function (req, res) {
 		var room = game.find(qdat.room);
 
 		if (room != 'invalid') {
-			room.requests.push({player: qdat.id, cards: qdat.cards});
+			room.requests.push({player: qdat.id, cards: JSON.parse(qdat.cards)});
 			res.write('requested');
 		} else {
 			res.write('invalid');
@@ -68,7 +71,7 @@ var game = {
 	rooms: [],
 	find: function (code) {
 		var result = 'invalid';
-		for(room of rooms) {
+		for(room of this.rooms) {
 			if (room.code = code) {
 				result = room;
 			}
@@ -80,11 +83,20 @@ var game = {
 // ============================================Play Loop============================================================================//
 var playLoop = function () {
 	//TODO: Implement updating of all games.
-	
+
 	for (room of game.rooms) {
-		console.log(`Room id: ${room.id}, Players: ${JSON.stringify(room.players)}`);
+		console.log(`Room id: ${room.id}, Players: ${JSON.stringify(room.players)}, Requests: ${JSON.stringify(room.requests)}`);
+
+		for (request of room.requests) {
+			if (request.player === room.state.turn.id) { // Allow play
+				room.pile.push(request.cards);
+			}
+		}
 	}
 }
+
+const playValid = function (request, ) {
+	
 
 var gameInterval = setInterval(playLoop, 1000);
 
@@ -98,6 +110,8 @@ var gameInterval = setInterval(playLoop, 1000);
 //			dealing: false,
 //			turn: {id: 0, name: ""},
 //			mod: 1, // For game: Singles, doubles, or triples
+//			pile: ['1d', '3h'],
+//			stock: ['5c', '8h'],
 //		},
-//		requests: [],
+//		requests: [{player: playerId, cards: ['1c', '3h']],
 //	}
